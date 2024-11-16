3d0980585031e0f902dff012542df6e0842b5996
junayeddd <junayed011@gmail.com>
2024-11-17 01:11:03 +0600

    added question.txt

--- main.c
+++ main.c
@@ -3,16 +3,53 @@
 #include <string.h>
 
 typedef struct {
-	char question[256];
+	char question[100];
 	char option[4][50];
 	char correctAns;
 } Question;
 
+
+/* loadQuestions();
+ * startGame();
+ * mainMenu();
+ * saveScore();
+*/
 int main(){
 	
-	Question q;
-	q.correctAns = 'a';
-	printf("%c", q.correctAns);
+
+	printf("Welcome to the quiz game\n");
+
+
+	/*Question *q = (Question *)malloc(sizeof(Question));
+	int count = 3;
+	int size = sizeof(Question);
+	
+	printf("You will be asked %d questions: \n", count);	
+	
+	strcpy(q[0].question, "What is 2+2?");
+	printf("Question: %s\n", q[0].question);
+	printf("size: %d\n", size);
+
+	free(q);
+	q = NULL;
+	*/
 	return 0;
 }
 
+	/*
+int loadQuestions(char *a){
+	FILE *file;
+	int n;
+	int i = 0;
+
+	file = fopen("question.txt", "r");
+	fscanf(file, "%d" , &n);
+	
+	
+
+	fclose(file);
+
+
+	return n;
+}
+*/
