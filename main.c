86f4f354eca3d8c0f935a6b3240aa2914efc118c
junayeddd <junayed011@gmail.com>
2024-11-29 22:56:01 +0600

    Loaded questions in the structure array

--- main.c
+++ main.c
@@ -2,35 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define MAX_QUES 6
+#define MAX_OPTIONS 4
+
 typedef struct {
-	char question[100];
+	char question[256];
 	char option[4][50];
 	char correctAns;
 } Question;
 
-void mainMenu();
+// dealaring functions
+int mainMenu();
+int starGame();
 
-/* loadQuestions();
+/* loadQuestions(struct, file);
  * startGame();
  * saveScore();
 */
 
 int main(){
 	
-	int total_ques = 10;
-	printf("\n\nYou will be asked %d questions!\n\n", total_ques);
 
-	Question questions[total_ques];
+	Question questions[MAX_QUES];
+	// int *q = questions;
 
+	FILE *fp = fopen("question.txt", "r");
+	if (fp == NULL)
+	{
+		printf("File not opened!");
+		return 1;
+	}
+	
+	int i, j;
+	for ( i = 0; i < MAX_QUES; i++)
+	{	
+		fgets(questions[i].question, 256, fp);
+		questions[i].question[strcspn(questions[i].question, "\n")] = '\0';
+
+		for ( j = 0; j < MAX_OPTIONS; j++)
+		{
+			fgets(questions[i].option[j], 50, fp);
+			questions[i].option[j][strcspn(questions[i].option[j], "\n")] = '\0';
+		}
+		
+		// fgets(&questions[i].correctAns, 1, fp);
+		fscanf(fp, "%c\n", &questions[i].correctAns);
+
+	}
+	
+	// Display Main Menu
 	mainMenu();
 
+
 	
+	fclose(fp);
 
 	return 0;
 }
+void startGame(){
+	printf("\n\nYou will be asked %d questions!\n\n", MAX_QUES);
 
 
-void mainMenu(){
+
+}
+
+
+int mainMenu(){
 
 	printf("\nWelcome to the quiz game!\n\n");
 
@@ -45,19 +83,24 @@ void mainMenu(){
 	printf("(1) Start Game\n");
 	printf("(2) Exit Game\n");
 
-	printf("Option: ");
+	printf("Enter your choice: ");
 	int flag = 0;
 	scanf("%d", &flag);
 	printf("\n");
 	
 	if(flag == 1){
 		printf("Starting game.....");
+		startGame();
+		
 	}
 	else if(flag == 2){
 		printf("Exiting game.....");
+		return 1;
 	}
 	else{
 		printf("Enter a valid option!");
+		return 1;
 	}
 
 }
+
