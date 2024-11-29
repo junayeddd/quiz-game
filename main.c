8aec94fb10fd6c9ba91ae10f362aecaa032e85d2
junayeddd <junayed011@gmail.com>
2024-11-29 11:25:25 +0600

    Created mainMenu UI

--- main.c
+++ main.c
@@ -8,48 +8,56 @@ typedef struct {
 	char correctAns;
 } Question;
 
+void mainMenu();
 
 /* loadQuestions();
  * startGame();
- * mainMenu();
  * saveScore();
 */
+
 int main(){
 	
+	int total_ques = 10;
+	printf("\n\nYou will be asked %d questions!\n\n", total_ques);
 
-	printf("Welcome to the quiz game\n");
+	Question questions[total_ques];
 
+	mainMenu();
 
-	/*Question *q = (Question *)malloc(sizeof(Question));
-	int count = 3;
-	int size = sizeof(Question);
 	
-	printf("You will be asked %d questions: \n", count);	
-	
-	strcpy(q[0].question, "What is 2+2?");
-	printf("Question: %s\n", q[0].question);
-	printf("size: %d\n", size);
 
-	free(q);
-	q = NULL;
-	*/
 	return 0;
 }
 
-	/*
-int loadQuestions(char *a){
-	FILE *file;
-	int n;
-	int i = 0;
 
-	file = fopen("question.txt", "r");
-	fscanf(file, "%d" , &n);
+void mainMenu(){
+
+	printf("\nWelcome to the quiz game!\n\n");
+
+    printf("  /\\_/\\  (\n");        
+    printf(" ( ^.^ ) _)\n");
+    printf("   \\\"/  (\n");       
+    printf(" ( | | )\n");
+    printf("(__d b__)\n");
+
+
+	printf("\n");
+	printf("(1) Start Game\n");
+	printf("(2) Exit Game\n");
+
+	printf("Option: ");
+	int flag = 0;
+	scanf("%d", &flag);
+	printf("\n");
 	
-	
+	if(flag == 1){
+		printf("Starting game.....");
+	}
+	else if(flag == 2){
+		printf("Exiting game.....");
+	}
+	else{
+		printf("Enter a valid option!");
+	}
 
-	fclose(file);
-
-
-	return n;
 }
-*/
